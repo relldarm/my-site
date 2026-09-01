55c766035c21ab34d2c7bd5624f6ef12ff03e9c3
Darrell Darmawan <ddarm@udel.edu>
2026-09-01 16:02:59 -0400

    Update display name

--- src/profile.ts
+++ src/profile.ts
@@ -16,7 +16,7 @@
 
 export const profile = {
   /** Your name as you want it to appear everywhere. */
-  displayName: "Darrell Darmawan",
+  displayName: "Darrell Darmawa",
 
   /** Your UD email — must match the one you signed into the course with. */
   email: "ddarm@udel.edu",
